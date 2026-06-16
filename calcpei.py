2e6c3147f71b99427852b1614d922ecbb69d5a0b
Tiago Farani <36167783+tlfarani@users.noreply.github.com>
2026-06-16 18:42:35 -0300

    Update calcpei.py

--- calcpei.py
+++ calcpei.py
@@ -167,9 +167,8 @@ local = st.sidebar.radio(
 # --- 4. ESTRATÉGIA DE RESPOSTA ---
 st.sidebar.header("4. Estratégia de Resposta")
 comprimento = st.sidebar.number_input("Comprimento do navio/fonte (m):", min_value=0.0, value=0.0)
-#st.sidebar.header("Contenção da mancha de óleo")
 # Nota informativa fixa da imagem anexa
-st.sidebar.caption("Contenção da mancha de óleo")
+st.sidebar.caption("Contenção da mancha de óleo: \n De acordo com o cálculo da capacidade efetiva diária")
 largura = st.sidebar.number_input("Largura do corpo hídrico (m):", min_value=0.0, value=0.0)
 velocidade = st.sidebar.number_input("Velocidade máxima da corrente (nós/m/s):", min_value=0.0, value=0.0)
 linha_protecao = st.sidebar.number_input("Comprimento da linha de proteção (m):", min_value=0.0, value=0.0)
